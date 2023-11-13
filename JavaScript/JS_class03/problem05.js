0d84f3da38ec5312be52f2b9a1f62a89d3a6b910
TanvirAlHasib <tanviralhasib1@gmail.com>
2023-11-13 18:39:07 +0600

    problem 05 has solved

---
+++ JavaScript/JS_class03/problem05.js
@@ -0,0 +1,6 @@
+let alphabet = "AaBbCcDdEeFfGgHhIJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
+let character = "i";
+
+if(alphabet.includes(character)) console.log("The",character,"character is in:", alphabet);
+
+else console.log("The",character,"character is not in:", alphabet);
