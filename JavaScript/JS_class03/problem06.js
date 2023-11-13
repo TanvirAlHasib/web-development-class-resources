12a8ce6f973441a5b71ee421999875db7301e9e4
TanvirAlHasib <tanviralhasib1@gmail.com>
2023-11-13 18:47:13 +0600

    problem 06 has solved

---
+++ JavaScript/JS_class03/problem06.js
@@ -0,0 +1,7 @@
+let temperature = 1;
+
+if(temperature >= 30) console.log("The weather is hot.");
+
+else if (temperature <= 25) console.log("The weather is cold.");
+
+else console.log("The weather is normal.");
