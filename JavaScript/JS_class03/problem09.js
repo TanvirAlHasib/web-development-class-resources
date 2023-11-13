75af489108ad6d4597daeffd904c6441a8a453cc
TanvirAlHasib <tanviralhasib1@gmail.com>
2023-11-13 19:15:49 +0600

    problem 09 has solved

---
+++ JavaScript/JS_class03/problem09.js
@@ -0,0 +1,5 @@
+let number1 = 30, number2 = 40;
+
+if((number1 > 30) && (number2 > 30)) console.log(number1+" "+number2+" is greater than 30.");
+
+else console.log(number1+" "+number2+" is not greater than 30.");
