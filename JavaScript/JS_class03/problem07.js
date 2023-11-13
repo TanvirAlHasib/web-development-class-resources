8570ae5b48bbb61b893d622e0097fb9998db3053
TanvirAlHasib <tanviralhasib1@gmail.com>
2023-11-13 18:56:59 +0600

    problem 07 has solved

---
+++ JavaScript/JS_class03/problem07.js
@@ -0,0 +1,7 @@
+let number1 = 50 , number2 = 400, number3 = 30;
+
+if((number1 > number2) && (number1 > number3)) console.log("The maximum numberis.", number1);
+
+else if((number2 > number1) && (number2 > number3)) console.log("The maximum numberis.", number2);
+
+else console.log("The maximum numberis.", number3);
