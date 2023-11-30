01481552f430244bd0d4a697844e509a66db6d4c
TanvirAlHasib <tanviralhasib1@gmail.com>
2023-11-30 17:46:31 +0600

    popup and add functional done

---
+++ JavaScript/JS_class08/calculator.js
@@ -0,0 +1,34 @@
+
+// variable declaretion and intro done
+
+let value1 = document.getElementById("input_one");
+let value2 = document.getElementById("input_two");
+let display = document.getElementById("tvDisplay");
+let open_popup = document.getElementById("popup");
+
+// function for ok button
+
+function close_popup(){
+
+    open_popup.classList.remove("open_popup");
+
+}
+
+
+// add button function
+
+
+function add(){
+
+    if ((value1.value) && (value2.value)) {
+
+        let sum = parseInt(value1.value) + parseInt(value2.value);
+        display.innerText = display.innerText+" "+sum;
+
+    } else{
+
+        open_popup.classList.add("open_popup");
+
+    }
+
+}
