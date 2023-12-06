593f58fbfae9e282973d6db93400bac6f27d3358
TanvirAlHasib <tanviralhasib1@gmail.com>
2023-12-06 16:27:47 +0600

    bug fixed for empty input

--- JavaScript/JS_Task_list/task.js
+++ JavaScript/JS_Task_list/task.js
@@ -6,6 +6,18 @@ const tasklist = document.getElementsByClassName("taskList");
 
 form.addEventListener("submit", (event)=>{
 
+    // reload prevent
     event.preventDefault();
+    
+    // input value taken to a variable
+    const input_value = input.value;
+
+    if(input_value){
+
+
+    } else{
+
+        alert("please input all values")
+    }
 
 })
