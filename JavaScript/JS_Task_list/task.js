8bbceffa4c1c792af857661bbb876132ca77b886
TanvirAlHasib <tanviralhasib1@gmail.com>
2023-12-06 15:58:13 +0600

    fixed reload problem by event.preventDefault function

---
+++ JavaScript/JS_Task_list/task.js
@@ -0,0 +1,11 @@
+// variable declaretion and intro
+
+const form = document.getElementById("new_task");
+const input = document.getElementById("input_task");
+const tasklist = document.getElementsByClassName("taskList");
+
+form.addEventListener("submit", (event)=>{
+
+    event.preventDefault();
+
+})
