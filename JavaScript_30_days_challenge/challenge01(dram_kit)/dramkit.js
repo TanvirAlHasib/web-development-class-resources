da2eedeebcb6d7dd2ad2b3cac22e2e7b3e47a1a4
TanvirAlHasib <tanviralhasib1@gmail.com>
2023-12-16 23:07:02 +0600

    music play and hit effect add done

---
+++ JavaScript_30_days_challenge/challenge01(dram_kit)/dramkit.js
@@ -0,0 +1,34 @@
+// variable declaretion and intro
+
+const keys = document.querySelectorAll(".key");
+
+
+// key listening and play audio
+
+window.addEventListener("keydown", (e) => {
+
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+
+    if (audio) {
+
+        // for every key hit make the audio rewind
+        audio.currentTime = 0;
+
+        // for playing the corresponding audio
+        audio.play();
+
+        // for adding effect after hiting the key
+        key.classList.add("hit_effect");
+
+
+    } else{
+
+        alert("music have only a,s,d,f,g,h,j,k,l in these keys");
+
+    }
+
+});
+
+// for disapearing the effect after transition
+
