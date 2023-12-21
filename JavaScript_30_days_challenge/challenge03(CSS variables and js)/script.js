37d2fb7011e23630a4b6235847a6135abb78a495
TanvirAlHasib <tanviralhasib1@gmail.com>
2023-12-21 20:55:43 +0600

    input tag modified in html file

--- JavaScript_30_days_challenge/challenge03(CSS variables and js)/script.js
+++ JavaScript_30_days_challenge/challenge03(CSS variables and js)/script.js
@@ -1,1 +1,13 @@
-console.log("test");
+// variable intro and declaretion
+
+const inputs = document.querySelectorAll(".element input");
+
+inputs.forEach(input => {
+
+    input.addEventListener("change", () => {
+
+        console.log(input.name, input.value);
+
+    });
+
+});
