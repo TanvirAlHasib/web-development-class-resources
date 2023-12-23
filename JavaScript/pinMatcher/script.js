26672bb664b6b5ba3990c102b52e84c7b67c7919
TanvirAlHasib <tanviralhasib1@gmail.com>
2023-12-23 19:14:22 +0600

    pin genarated section's variable intro and declaretion done

---
+++ JavaScript/pinMatcher/script.js
@@ -0,0 +1,15 @@
+// variable intro and declaretion 
+
+const genaratedPin = document.getElementById("genaratedPin");
+const pinGenarateButton = document.getElementById("pinGenarateButton");
+
+
+// pin genarated section start
+
+
+
+
+
+
+
+//pin genarated section end
