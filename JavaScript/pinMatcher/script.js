6bae58192b3b403ef617bcff4f7e9b3fba10f653
TanvirAlHasib <tanviralhasib1@gmail.com>
2023-12-23 19:31:42 +0600

    pin genarated section done in javaScript

--- JavaScript/pinMatcher/script.js
+++ JavaScript/pinMatcher/script.js
@@ -2,13 +2,31 @@
 
 const genaratedPin = document.getElementById("genaratedPin");
 const pinGenarateButton = document.getElementById("pinGenarateButton");
+let genaratedPinCount = 3;
 
 
 // pin genarated section start
 
 
 
+pinGenarateButton.addEventListener("click",randomPinGenaration);
 
+
+//function for pin genarating
+function randomPinGenaration() {
+    const randomNumber = Math.floor((Math.random()*9000) + 1000);
+    // to check that has user already genarated pin or not
+    if (genaratedPinCount > 0) {
+        
+        genaratedPin.value = randomNumber;
+        genaratedPinCount = genaratedPinCount - 1;
+
+    } else{
+
+        alert("you have already genarated pins three times");
+
+    }
+}
 
 
 
