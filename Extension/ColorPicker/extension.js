25ac421f88475e3ce5ad33020c9b2cd63265bbbd
TanvirAlHasib <tanviralhasib1@gmail.com>
2024-01-05 20:27:42 +0600

    variable declaretion and intro done

--- Extension/ColorPicker/extension.js
+++ Extension/ColorPicker/extension.js
@@ -1,1 +1,11 @@
-console.log("hello");
+//variable and declaretion
+const picker_button = document.getElementById("pick");
+const color_demo = document.querySelector(".color_demo");
+const color_value = document.querySelector(".color_value");
+const clear_all = document.getElementById("clear_all");
+
+
+picker_button.addEventListener("click", () => {
+
+    alert("hello it is working");
+})
