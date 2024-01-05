d47225fc10dc00ee6c14e1e7f945f1f3204b561b
TanvirAlHasib <tanviralhasib1@gmail.com>
2024-01-05 21:26:03 +0600

    basic color adding done

--- Extension/ColorPicker/extension.js
+++ Extension/ColorPicker/extension.js
@@ -1,11 +1,25 @@
 //variable and declaretion
 const picker_button = document.getElementById("pick");
-const color_demo = document.querySelector(".color_demo");
-const color_value = document.querySelector(".color_value");
 const clear_all = document.getElementById("clear_all");
+const picked_color_lists = document.querySelector(".picked_color_lists");
+
+//defualt color list section hide
+picked_color_lists.style.display = "none";
+
+//eye droper starting event for picking color
+picker_button.addEventListener("click", async() => {
+
+    //eye dropper method initialized
+    const eyeDropper = new EyeDropper();
+    const colour = await eyeDropper.open();
+
+    //after picking color display will be visible
+    picked_color_lists.style.display = "flex"
+
+    picked_color_lists.innerHTML += `<li id="color">
+    <div class="color_demo" style="background-color: ${colour.sRGBHex};"></div>
+    <span class="color_value">${colour.sRGBHex}</span>
+    </li>`;
 
 
-picker_button.addEventListener("click", () => {
-
-    alert("hello it is working");
-})
+});
