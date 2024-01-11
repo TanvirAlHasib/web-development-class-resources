42bbb184b4b32489aa0847fd6b8eadb9bb1fd092
TanvirAlHasib <tanviralhasib1@gmail.com>
2024-01-11 14:17:26 +0600

    basic js done for map

---
+++ Frontend_mentor/IP tracker project/ip_address_tracker.js
@@ -0,0 +1,8 @@
+const map = L.map('map').setView([25.184, 89.1889], 18);
+
+const satelliteView = L.tileLayer('http://{s}.google.com/vt?lyrs=s,h&x={x}&y={y}&z={z}', {
+    maxZoom: 20,
+    subdomains:['mt0','mt1','mt2','mt3']
+});
+
+satelliteView.addTo(map);
