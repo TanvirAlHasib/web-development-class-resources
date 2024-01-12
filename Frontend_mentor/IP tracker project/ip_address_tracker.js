0ee8976d7bc174333f4fd7c8730ef0956152603d
TanvirAlHasib <tanviralhasib1@gmail.com>
2024-01-12 21:30:32 +0600

    dom intro done

--- Frontend_mentor/IP tracker project/ip_address_tracker.js
+++ Frontend_mentor/IP tracker project/ip_address_tracker.js
@@ -1,3 +1,15 @@
+//getting location by IP Geolocation API section start
+
+//intro with dom
+const ip = document.getElementById("ip");
+const location = document.getElementById("location");
+const time_zone = document.getElementById("time_zone");
+const isp = document.getElementById("isp");
+
+//getting location by IP Geolocation API section end
+
+//map section start
+
 const map = L.map("map").setView([25.184, 89.1889], 18);
 
 const satelliteView = L.tileLayer(
@@ -10,7 +22,10 @@ const satelliteView = L.tileLayer(
 
 satelliteView.addTo(map);
 
-//for custom marker
+// map section end
+
+//custom marker section start
+
 const myIcon = L.icon({
   iconUrl: "marker.png",
   iconSize: [40, 60],
@@ -19,3 +34,5 @@ const myIcon = L.icon({
 const marker = L.marker([25.184, 89.1889], { icon: myIcon });
 
 marker.addTo(map);
+
+// custom marker section end
