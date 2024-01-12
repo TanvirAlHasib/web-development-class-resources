4617164e4edee5ba3d499dc25d1dd3b987600873
TanvirAlHasib <tanviralhasib1@gmail.com>
2024-01-12 21:08:23 +0600

    custom marker added done

--- Frontend_mentor/IP tracker project/ip_address_tracker.js
+++ Frontend_mentor/IP tracker project/ip_address_tracker.js
@@ -1,12 +1,21 @@
-const map = L.map('map').setView([25.184, 89.1889], 18);
+const map = L.map("map").setView([25.184, 89.1889], 18);
 
-const satelliteView = L.tileLayer('http://{s}.google.com/vt?lyrs=s,h&x={x}&y={y}&z={z}', {
+const satelliteView = L.tileLayer(
+  "http://{s}.google.com/vt?lyrs=s,h&x={x}&y={y}&z={z}",
+  {
     maxZoom: 20,
-    subdomains:['mt0','mt1','mt2','mt3']
-});
+    subdomains: ["mt0", "mt1", "mt2", "mt3"],
+  }
+);
 
 satelliteView.addTo(map);
 
-const marker = L.marker([25.184, 89.1889]);
+//for custom marker
+const myIcon = L.icon({
+  iconUrl: "marker.png",
+  iconSize: [40, 60],
+});
+
+const marker = L.marker([25.184, 89.1889], { icon: myIcon });
 
 marker.addTo(map);
